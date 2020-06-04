9430341ac310d23d3a4315a00f9421909ad435fe
dmytoli <dima.lytvynchuk@gmail.com>
2020-06-04 07:42:36 +0300

    БЦС Поділля 04.06

--- js/custom.js
+++ js/custom.js
@@ -3,13 +3,15 @@ $(document).ready(function(){
 $('.main-navigation .mobile-buttonMenu').on('click', function(){
 	$('.main-header__menu-container').toggleClass('header-menu--close');
 	$('.index-main').toggleClass('index-main--mobile');
-})
+});
 
 $(function() {
     if(!location.href.match(/index/)) {
         $(".parallax-window").show();
     }
 
-});        
+});  
+
+
 
 });

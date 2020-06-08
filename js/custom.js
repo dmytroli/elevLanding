00bb86b5a36ab09f38560b274d1d71c385a6acb2
dmytoli <dima.lytvynchuk@gmail.com>
2020-06-08 23:44:15 +0300

    Версія сайту 08.06.20

--- js/custom.js
+++ js/custom.js
@@ -13,5 +13,33 @@ $(function() {
 });  
 
 
+// $('.production__item').mouseenter(function(){
+// 	$('.production__item').find('.animated').each(function(i, el){
+// 		console.log($(this));
+// 		if ($(this).hasClass('fadeIn')) {
+// 			$(this).removeClass('fadeIn');
+// 		}
+// 		console.log($(this));
+// 	});
+	
+// 	$(this).find('.animated').addClass('fadeIn');
+// });
+
+$('.production__item').mouseenter(function(){
+	if(!($(this).find('.production__text').hasClass('animated'))) {
+		$(this).find('.production__text').addClass('animated');
+	}
+	$(this).find('.production__text').addClass('fadeIn');
+})
+$('.production__item').mouseleave(function(){
+	$(this).find('.production__text').removeClass('fadeIn');
+});
+
+
+
+// $('.production__item').mouseout(function(){
+// 	$(this).find('.production__text').removeClass('wow zoomIn');
+// });
+
 
 });
